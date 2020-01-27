7e54bb5fb3dec0df308c6f4bcf4441032ccb6420
e.vasilevich <e.vasilevich@corp.profigroup.by>
2020-01-27 11:04:00 +0300

    * make clean architecture + gradle setup

--- presentation/src/main/java/com/pandasby/cats/CatsActivity.kt
+++ presentation/src/main/java/com/pandasby/cats/CatsActivity.kt
@@ -1,5 +1,12 @@
 package com.pandasby.cats
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class CatsActivity : AppCompatActivity()
+class CatsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cats)
+    }
+}
